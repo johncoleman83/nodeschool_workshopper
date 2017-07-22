c5ceaefc6bde76faac339709fa9ba142a44bac38
johncoleman83 <lcsw@davidjohncoleman.com>
2017-07-22 10:23:04 -0700

    function reading file with ASYNC function

---
+++ learnyounode/4.js
@@ -0,0 +1,13 @@
+var fs = require('fs')
+var file = process.argv[2];
+fs.readFile(file, "utf8", function(err, data) {
+    if (err) {
+        console.log(err);
+    } else {
+		printLineCount(data);
+	}
+});
+
+function printLineCount(data) {
+    console.log((data.match(/\n/g) || []).length);
+};

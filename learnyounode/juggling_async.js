86c27a62ec957d81525986081d814aa670e104fe
johncoleman83 <lcsw@davidjohncoleman.com>
2017-08-02 16:12:24 -0700

    completed juggling asynchronous functions using callback functions

--- learnyounode/juggling_async.js
+++ learnyounode/juggling_async.js
@@ -3,12 +3,14 @@
  */
 
 var http = require('http');
-var allResponses = [];
+var allResponses = {};
 var jobsFinished = 0;
+var inputurl;
 
 function printAll() {
   for (var i = 0; i < 3; i++) {
-    console.log(allResponses[i]);
+    inputurl = process.argv[2 + i].split(':')[2];
+    console.log(allResponses[inputurl]);
   };
 };
 
@@ -17,19 +19,19 @@ function callback(response) {
   response.setEncoding('utf8');
 
   var urldata = [];
+  var url = (response.req._header.split('\n')[1]).split(':')[2].slice(0, -1);
 
   response.on('data', function (data) {
     urldata.push(data);
   });
 
   response.on('end', function () {
-    allResponses.push(urldata.join(''));
+    allResponses[url] = urldata.join('');
     jobsFinished++;
     if (jobsFinished == 3) {
       printAll();
     };
   });
-
 };
 
 for (var j = 0; j < 3; j++) {

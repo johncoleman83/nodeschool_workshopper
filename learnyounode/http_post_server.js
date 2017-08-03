a7cec94c393ba0b2d85760372b44bf9d0663ce72
johncoleman83 <lcsw@davidjohncoleman.com>
2017-08-02 21:41:40 -0700

    http request for post

---
+++ learnyounode/http_post_server.js
@@ -0,0 +1,28 @@
+/**
+ * creating an HTTP file server with Node.js that
+ * returns UPCASE for all POST requests
+ */
+
+var fs = require('fs');
+var http = require('http');
+var port = process.argv[2];
+
+var server = http.createServer(
+  function (request, response)
+  {
+    if (request.method == 'POST')
+    {
+      var body = '';
+      request.on('data', function (data) {
+	body += data;
+      });
+
+      request.on('end', function () {
+	body = body.toUpperCase();
+	response.write(body);
+	response.end();
+      });
+    }
+  });
+
+server.listen(port);

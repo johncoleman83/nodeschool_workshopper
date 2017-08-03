7ed9fde86a71eedda17bfd1a73078ad373f954b5
johncoleman83 <lcsw@davidjohncoleman.com>
2017-08-02 19:37:03 -0700

    created a server to listen on port from input arguments

--- learnyounode/time_server.js
+++ learnyounode/time_server.js
@@ -6,7 +6,29 @@ var net = require('net');
 var port = process.argv[2];
 var http = require('http');
 
-var server = net.createServer(function (socket) {
-  socket.write('Hello');
-});
+function fetchDate()
+{
+  var d = new Date();
+  var dmon = d.getMonth() + 1;
+  var dd = d.getDate();
+  var dh = d.getHours();
+  var dmin = d.getMinutes();
+
+  dmon = dmon < 10 ? '-0' + dmon : '-' + dmon;
+  dd = dd < 10 ? '-0' + dd : '-' + dd;
+  dh = dh < 10 ? ' 0' + dh : ' ' + dh;
+  dmin = dmin < 10 ? ':0' + dmin : ':' + dmin;
+
+  var datetime = '' + d.getFullYear() + dmon + dd + dh + dmin + '\n';
+
+  return datetime;
+}
+
+var server = net.createServer(
+  function (socket)
+  {
+    socket.write(fetchDate());
+    socket.end();
+  });
+
 server.listen(port);
